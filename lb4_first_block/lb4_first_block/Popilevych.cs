43336b555c859634f2e9d905c32ca5ba3569db3a
dan436366 <mavpadan@gmail.com>
2023-04-23 21:53:57 +0300

    example commit

---
+++ lb4_first_block/lb4_first_block/Popilevych.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lb4_popilevych_block1
+{
+    public partial class Popilevych 
+    {
+        public static int[] EraseB(int[] a, int idx)
+        {
+            if (a == null || idx < 1 || idx > a.Length)
+            {
+                Console.WriteLine("Видалити неможливо, " +
+                "індекс поза допустимими межами");
+                return a;
+            }
+            else
+            {
+                int[] b = new int[a.Length - 1];
+                for (int i = 0; i < idx - 1; i++)
+                {
+                    b[i] = a[i];
+                }
+                for (int i = idx; i < a.Length; i++)
+                {
+                    b[i - 1] = a[i];
+                }
+                return b;
+            }
+        }
+        public static void DeleteOddElem(ref int[] array)
+        {
+            for (int i = array.Length; i > 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    array = EraseB(array, i);
+                }
+            }
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lb4_first_block/lb4_first_block/Program.cs
@@ -0,0 +1,118 @@
+﻿using lb4_zaichyk_block1;
+using lb4_popilevych_block1;
+using lb4_krasilnikov_block1;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Lab4
+{
+    internal class example1
+    {
+        public static void Choose_The_Way_To_Fill(int countOfElements, ref int[] array)
+        {
+            Console.WriteLine("Натиснiть 1, якщо бажаєте заповнити масив випадковим чином");
+            Console.WriteLine("Натиснiть 2, якщо бажаєте заповнити масив вручну в одному рядку");
+            int choiceOfInput = Convert.ToInt32(Console.ReadLine());
+            switch (choiceOfInput)
+            {
+                case 1:
+                    InputRandom(countOfElements, ref array);
+                    Console.Write("Початковий масив: ");
+                    for (int i = 0; i < countOfElements; i++)
+                    {
+                        Console.Write(array[i] + " ");
+                    }
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    Console.Write("Введiть масив:");
+                    Input_ByHand(countOfElements, ref array);
+                    break;
+            }
+        }
+        static void InputRandom(int countOfElements, ref int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < countOfElements; i++)
+            {
+                array[i] = random.Next(-100, 100);
+            }
+        }
+        static void Input_ByHand(int countOfElements, ref int[] array)
+        {
+            string[] rowOfArray = Console.ReadLine().Trim().Split();
+            for (int i = 0; i < countOfElements; i++)
+            {
+                array[i] = Convert.ToInt32(rowOfArray[i]);
+            }
+        }
+
+        static void PrintArray(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                Console.WriteLine("Знищено всi елементи масиву");
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Block_1()
+        {
+            Console.WriteLine("Введiть прiзвище студента, варiант якого хочете виконати. Для завершення виконання варiанту програми введiть 0. ");
+            string choiceOfStudent = Console.ReadLine().ToLower();
+            Console.Write("Введiть к-сть елементiв масиву: ");
+            int countOfElements = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[countOfElements];
+            Choose_The_Way_To_Fill(countOfElements, ref array);
+            do
+            {
+                switch (choiceOfStudent)
+                {
+                    case "красільніков":
+                        Krasilnikov.ExtremeMinMax(ref array);
+                        Console.WriteLine("Результат:");
+                        PrintArray(array);
+                        break;
+                    case "попілевич":
+                        Popilevych.DeleteOddElem(ref array);
+                        Console.WriteLine("Результат:");
+                        PrintArray(array);
+                        break;
+                    case "зайчук":
+                        Lilia.Rishennya(ref array);
+                        Console.WriteLine("Результат:");
+                        PrintArray(array);
+                        break;
+                    default:
+                        Console.WriteLine("Спробуйте ще раз");
+                        break;
+                }
+                Console.WriteLine("Введiть прiзвище студента, варiант якого хочете виконати. Для завершення виконання варiанту програми введiть 0. ");
+                choiceOfStudent = Console.ReadLine().ToLower();
+                if (choiceOfStudent == "красільніков" || choiceOfStudent == "попілевич" || choiceOfStudent == "зайчук")
+                {
+                    Console.WriteLine("Бажаєте створити новий масив?(Так/Нi)");
+                    string answer = Console.ReadLine().ToLower();
+                    if (answer == "так")
+                    {
+                        Console.Write("Введiть к-сть елементiв: ");
+                        countOfElements = Convert.ToInt32(Console.ReadLine());
+                        array = new int[countOfElements];
+                        Choose_The_Way_To_Fill(countOfElements, ref array);
+                    }
+                }
+            } while (choiceOfStudent != "0");
+        }
+
+        public static void Main(string[] args)
+        {
+            Block_1();
+        }
+    }
+}
